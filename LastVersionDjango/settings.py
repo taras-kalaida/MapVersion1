5384d7988594dbfba974c75ed81dd9dca05ccd99
anrikolo <anrikolo413393@gmail.com>
2021-12-11 21:56:59 +0200

    Add template for map First map

--- LastVersionDjango/settings.py
+++ LastVersionDjango/settings.py
@@ -77,12 +77,8 @@ WSGI_APPLICATION = 'LastVersionDjango.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'mapdb',
-        "PASSWORD": "Taras679",
-        "USER": "postgres",
-        "HOST": "localhost",
-        "PORT": "5432"
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3'
     }
 }
 
