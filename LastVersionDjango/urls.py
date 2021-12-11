a755a6c2866ed3d17288b28f4a93348b59a3f6c3
Inspiration679 <taraskalaida@gmail.com>
2021-12-11 21:41:21 +0200

    Add templates

--- LastVersionDjango/urls.py
+++ LastVersionDjango/urls.py
@@ -17,7 +17,10 @@ from django.contrib import admin
 from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
+from ShowMap.views import ShowMap
 
 urlpatterns = [
+
                   path('admin/', admin.site.urls),
-              ] + static(settings.MEDIA_ROOT, document_root=settings.MEDIA_ROOT)
+                  path("", ShowMap.as_view(), name="show_map"),
+              ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
