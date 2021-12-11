c42cda541664246a9718816186f0aea66b22f6d7
anrikolo <anrikolo413393@gmail.com>
2021-12-12 00:38:09 +0200

    Leaflet init map and marker

--- static/js/map.js
+++ static/js/map.js
@@ -1,12 +1,22 @@
-navigator.geolocation.getCurrentPosition(
-    function (position) {
-        latitude = position.coords.latitude;
-        longitude = position.coords.longitude;
-        Init(latitude, longitude);
-    }
-);
+var map = L.map('map').setView([50.449279, 30.523132], 20);
 
-function Init(latitude, longitude) {
-    const CurentPosition = [longitude, latitude]
-    const Kiev = [30.523132, 50.449279];
-}
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(map);
+
+L.marker([50.449279, 30.523132]).addTo(map)
+    .bindPopup('Test Marker.<br> AAAA')
+    .openPopup();
+
+// navigator.geolocation.getCurrentPosition(
+//     function (position) {
+//         latitude = position.coords.latitude;
+//         longitude = position.coords.longitude;
+//         Init(latitude, longitude);
+//     }
+// );
+//
+// function Init(latitude, longitude) {
+//     const CurentPosition = [longitude, latitude]
+//     const Kiev = [30.523132, 50.449279];
+// }
