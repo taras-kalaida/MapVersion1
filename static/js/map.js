d4cca2afa0bd9150733c5754df209904ccecfdb4
anrikolo <anrikolo413393@gmail.com>
2021-12-12 03:59:46 +0200

    tnk for fix Taras love you

--- static/js/map.js
+++ static/js/map.js
@@ -1,3 +1,4 @@
+
 navigator.geolocation.getCurrentPosition(
     function (position) {
         latitude = position.coords.latitude;
@@ -27,20 +28,27 @@ function Init(latitude, longitude) {
     iconAnchor: [22, 94],
     popupAnchor: [-3, -76]
 });
+        let a,b ;
+        for (let i of JSON.parse(arr)){
+        a = i.fields.image;
+        b = i.fields.wiki;
+    }
+        console.log(b)
+        let n = "media/"+a;
 
 let mark = L.marker([50.449279, 30.523132], {icon: myIcon}).addTo(map)
-         .bindPopup("<h1>Taras</h1><br> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Delectus est, eum facere facilis illo ipsam iste maiores officia optio quam quibusdam quidem, sed soluta. Aperiam eveniet modi praesentium saepe sapiente.<img style='height: 200px;width: 300px' src='/static/js/kafedralnyj-sobor.jpg' > ")
-        var myIcon = L.icon({
-    iconUrl: '/static/js/man.png',
-    iconSize: [50, 95],
-    iconAnchor: [22, 94],
-    popupAnchor: [-3, -76]
-});
-
-let mark1 = L.marker([50.449279, 30.529140], {
-    icon: myIcon,
-    draggable: true
-        }).addTo(map).bindPopup("<h1>Taras</h1><br> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Delectus est, eum facere facilis illo ipsam iste maiores officia optio quam quibusdam quidem, sed soluta. Aperiam eveniet modi praesentium saepe sapiente.<img style='height: 200px;width: 300px' src='/static/js/kafedralnyj-sobor.jpg' > ")
+         .bindPopup(`<h1>Taras</h1 ><br>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Delectus est, eum facere facilis illo ipsam iste maiores officia optio quam quibusdam quidem, sed soluta. Aperiam eveniet modi praesentium saepe sapiente.<img style='height: 200px;width: 300px' src=${n}><br><a href=${b}>aaaaaaaaaaaa</a> `)
+//         var myIcon = L.icon({
+//     iconUrl: '/static/js/man.png',
+//     iconSize: [50, 95],`
+//     iconAnchor: [22, 94],
+//     popupAnchor: [-3, -76]
+// });
+//
+// let mark1 = L.marker([50.449279, 30.529140], {
+//     icon: myIcon,
+//     draggable: true
+//         }).addTo(map).bindPopup("<h1>Taras</h1><br> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Delectus est, eum facere facilis illo ipsam iste maiores officia optio quam quibusdam quidem, sed soluta. Aperiam eveniet modi praesentium saepe sapiente.<img style='height: 200px;width: 300px' src='/static/js/kafedralnyj-sobor.jpg' > ")
 
     }
 
