210d7e0a02ac3f4bfc93b7a17e99636e86e7bf64
Inspiration679 <taraskalaida@gmail.com>
2021-12-12 03:12:30 +0200

    Change db fields

--- ShowMap/models.py
+++ ShowMap/models.py
@@ -4,3 +4,7 @@ from django.db import models
 class Monuments(models.Model):
     id = models.IntegerField(primary_key=True, unique=True)
     title = models.CharField(max_length=50)
+    description = models.TextField()
+    image = models.ImageField(upload_to="images/monuments_image")
+    wiki = models.CharField(max_length=100)
+    slug = models.IntegerField( unique=True)
