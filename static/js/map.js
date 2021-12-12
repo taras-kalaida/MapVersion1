1adbca2e10e5614d0785f028cad6ad089e59de85
anrikolo <anrikolo413393@gmail.com>
2021-12-12 16:29:39 +0200

    Find Person

--- static/js/map.js
+++ static/js/map.js
@@ -1,6 +1,6 @@
 
 function StartMap(){
-        let map = L.map('map').setView([50.449279, 30.523132], 18);
+        let map = L.map('map').setView([0, 0], 6);
 
         L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
                 attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
@@ -29,10 +29,17 @@ function CurrentPositionOnMap(position){
     let long = position.coords.longitude;
     let accuracy = position.coords.accuracy;
 
+    if(marker1){
+        map.removeLayer(marker1)
+    }
+
+    if(circle1){
+        map.removeLayer(circle1)
+    }
 
       marker1 = L.marker([lat, long])
-      circle1 = L.circle([lat,long],{radius:accuracy})
+      circle1 = L.circle([lat,long],{radius:500})
     let PersonPositionGrup = L.featureGroup([marker1,circle1]).addTo(map)
-    map.fitBounds(PersonPositionGrup.getBounds())
+    //map.fitBounds(PersonPositionGrup.getBounds())
      console.log(lat, long)
 }
